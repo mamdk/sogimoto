ef4fc9bdac85bf4644e4779e060174e1788602c2
mhmk <mhm1379mhm3@gmail.com>
2025-07-25 18:54:00 +0330

    feat: header section

---
+++ frontend/src/components/section/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="bg-blue-500 text-white py-4 px-6 shadow-md mb-4">
+            <div className="container mx-auto">
+                <h1 className="text-2xl font-bold">Sugimoto</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
